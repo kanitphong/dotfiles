b305849336465fefe686f4ae25ee9f2409129c32
iArt <iart@aboutdev.io>
2025-01-31 15:44:08 +0700

    Add starship config

--- .config/zsh/custom.zsh
+++ .config/zsh/custom.zsh
@@ -8,6 +8,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 # Starship
+export STARSHIP_CONFIG=$HOME/.config/starship/config.toml
 eval "$(starship init zsh)"
 
 # # Load Git completion
